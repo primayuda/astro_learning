55f5e27e9eaf0f9579d4289a519fd3ceb9854529
Aprita Primayuda <aprimayu@cisco.com>
2026-06-14 19:14:53 +0700

    Using content collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,17 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: (await getCollection("blog")).map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      link: `/posts/${post.id}/`,
+      pubDate: post.data.pubDate,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
