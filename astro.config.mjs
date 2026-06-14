e74b837a03638648665d303be900ea960b7c0dc1
Aprita Primayuda <aprimayu@cisco.com>
2026-06-14 14:41:27 +0700

    Final part of tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://primsastroblog.netlify.app/"
+  site: "https://primsastroblog.netlify.app/",
+  integrations: [preact()]
 });
